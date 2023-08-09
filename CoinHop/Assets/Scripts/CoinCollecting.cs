6a6c871ca4e4e6435a489aee1ab62332a4d1409f
Keean Ferreira <keeanferreira07@gmail.com>
2023-08-09 20:07:35 +0200

    Add game files
    Add game files. I believe this includes first level and menu's.

---
+++ CoinHop/Assets/Scripts/CoinCollecting.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCollecting : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject platform;
+    private CoinSpawner coinSpawner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinSpawner = platform.GetComponent<CoinSpawner>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "coin")
+        {
+            //Debug.Log("Got coin");
+            coinSpawner.RemoveCoin(collider);
+        }
+    }
+}
---
+++ CoinHop/Assets/Scripts/CoinSpawner.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using TMPro;
+
+public class CoinSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject coin;
+
+    public List<GameObject> coinsList;
+
+    private GameObject child;
+
+    public int score;
+
+    public TMP_Text pointsText;
+
+    private float rotatespeed = 120;
+
+    private Vector3 rotationDirection = new Vector3(0, 0, 1);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinsList = new List<GameObject>();
+
+        for (int i = 0; i < 8; i++)
+        {
+            child = transform.GetChild(i).gameObject;
+            coinsList.Add(child);
+        }
+
+        InvokeRepeating("CheckCoins", 1f, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < coinsList.Count; i++)
+        {
+            coinsList[i].transform.Rotate(rotatespeed * rotationDirection * Time.deltaTime);
+        }
+        pointsText.text = "Points: " + score;
+    }
+
+    void CheckCoins()
+    {
+        bool remaining = false;
+
+        for (int i = 0; i < coinsList.Count; i++)
+        {
+            if (coinsList[i].activeSelf == true)
+            {
+                remaining = true;
+            }
+        }
+
+        if (remaining == false)
+        {
+            for (int i = 0; i < coinsList.Count; i++)
+            {
+                coinsList[i].SetActive(true);
+            }
+        }
+    }
+    /***
+     * SpawnCoin is used to spawn in coins at specific coordinates
+     */
+    private void SpawnCoin()
+    {
+        //// Create Coordinates
+        //float xpos = (float)rnd.Next(-2, 3);
+        //float zpos = (float)rnd.Next(-2, 3);
+        //float ypos = 0.35f;
+
+        //// Create Position Vector
+        //Vector3 coinPos = new Vector3(xpos, ypos, zpos);
+
+        //// Create Rotation Vector
+        //Vector3 coinRotation = new Vector3(90, 0, 0);
+
+        //// Create new coin
+        //GameObject newCoin = Instantiate(coin, transform.position + coinPos, coin.transform.rotation);
+
+        //// Give coin name
+        //newCoin.name = "Coin" + (coinsList.Count + 1);
+
+        //// Add coin to coins list
+        //coinsList.Add(newCoin);
+    }
+
+    public void RemoveCoin(Collider collider)
+    {
+        for (int i = 0; i < coinsList.Count;i++)
+        {
+            if (collider.gameObject == coinsList[i])
+            {
+                coinsList[i].SetActive(false);
+                score++;
+            }
+        }
+    }
+}
